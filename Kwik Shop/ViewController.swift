93b30604a6289338826172c592bfd299162c0056
Adrian Kretschmer <adrian@Adrians-Mini.fritz.box>
2015-07-26 13:01:27 +0200

    initial commit from local git

---
+++ Kwik Shop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Kwik Shop
+//
+//  Created by Adrian Kretschmer on 26.07.15.
+//  Copyright © 2015 FAU-Inf2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
