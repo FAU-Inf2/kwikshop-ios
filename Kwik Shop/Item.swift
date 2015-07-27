395bf0f92ea02a575f75bb069ede44f138159490
Adrian Kretschmer <adrian@kremoe.de>
2015-07-27 11:43:12 +0200

    Added basic version of Shopping List

---
+++ Kwik Shop/Item.swift
@@ -0,0 +1,9 @@
+//
+//  Item.swift
+//  Kwik Shop
+//
+//  Created by Adrian Kretschmer on 27.07.15.
+//  Copyright © 2015 FAU-Inf2. All rights reserved.
+//
+
+import Foundation
---
+++ Kwik Shop/ShoppingList.swift
@@ -0,0 +1,23 @@
+//
+//  ShoppingList.swift
+//  Kwik Shop
+//
+//  Created by Adrian Kretschmer on 27.07.15.
+//  Copyright © 2015 FAU-Inf2. All rights reserved.
+//
+
+import Foundation
+
+class ShoppingList {
+    let id : Int
+    let name : String
+    var sortType : Int
+    var items : [Item]
+    
+    init (id : Int, name : String, sortType : Int) {
+        self.id = id;
+        self.name = name;
+        self.sortType = sortType
+        self.items = [Item]()
+    }
+}
