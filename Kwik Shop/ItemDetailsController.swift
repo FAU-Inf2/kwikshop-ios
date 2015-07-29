201aac37727416e8280af5e09b66b25041a7dbef
Adrian Kretschmer <adrian@kremoe.de>
2015-07-29 11:58:10 +0200

    Created basic ItemDetailsController, added Outlets for UI elements

---
+++ Kwik Shop/ItemDetailsController.swift
@@ -0,0 +1,32 @@
+//
+//  ItemDetailsController.swift
+//  Kwik Shop
+//
+//  Created by Adrian Kretschmer on 29.07.15.
+//  Copyright (c) 2015 FAU-Inf2. All rights reserved.
+//
+
+import UIKit
+
+class ItemDetailsController : UIViewController {
+    // MARK: Properties
+    @IBOutlet weak var itemNameTextField: UITextField!
+    @IBOutlet weak var amountLabel: UILabel!    
+    @IBOutlet weak var amountTextField: UITextField!
+    @IBOutlet weak var unitPicker: UIPickerView!
+    @IBOutlet weak var brandLabel: UILabel!
+    @IBOutlet weak var brandTextField: UITextField!
+    @IBOutlet weak var commentLabel: UILabel!
+    @IBOutlet weak var commentTextField: UITextField!
+    @IBOutlet weak var cancelButton: UIBarButtonItem!
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+    @IBOutlet weak var deleteButton: UIBarButtonItem!
+    
+    
+    // MARK: UIViewController
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: Actions
+}
