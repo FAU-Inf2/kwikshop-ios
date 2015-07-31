db284a9da1dc77df3ac5baf00b83fc447539f58c
Adrian Kretschmer <adrian@kremoe.de>
2015-07-31 20:25:30 +0200

    Added Unit and Group classes

---
+++ Kwik Shop/Group.swift
@@ -0,0 +1,20 @@
+//
+//  Group.swift
+//  Kwik Shop
+//
+//  Created by Adrian Kretschmer on 31.07.15.
+//  Copyright (c) 2015 FAU-Inf2. All rights reserved.
+//
+
+import Foundation
+
+class Group {
+    let id : Int
+    let name : String
+    
+    init(id : Int, name : String){
+        self.id = id
+        self.name = name
+    }
+    
+}
---
+++ Kwik Shop/Unit.swift
@@ -0,0 +1,26 @@
+//
+//  Unit.swift
+//  Kwik Shop
+//
+//  Created by Adrian Kretschmer on 31.07.15.
+//  Copyright (c) 2015 FAU-Inf2. All rights reserved.
+//
+
+import Foundation
+
+class Unit {
+    let id : Int
+    let name : String
+    let shortName : String?
+    
+    init(id : Int, name : String, shortName : String){
+        self.id = id
+        self.name = name
+        self.shortName = shortName
+    }
+    
+    convenience init (id : Int, name : String) {
+        self.init(id: id, name: name)
+    }
+    
+}
