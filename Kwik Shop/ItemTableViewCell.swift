ca04912112ab42755b0a7b22fd4327b7dff3c37c
Adrian Kretschmer <adrian@kremoe.de>
2015-08-06 12:52:36 +0200

    Added a basic custom table view cell, use it in Shopping List View and feed some dummy data

---
+++ Kwik Shop/ItemTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  ItemTableViewCell.swift
+//  
+//
+//  Created by Adrian Kretschmer on 06.08.15.
+//
+//
+
+import UIKit
+
+class ItemTableViewCell: UITableViewCell {
+
+    //@IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Kwik Shop/ShoppingListController.swift
+++ Kwik Shop/ShoppingListController.swift
@@ -8,7 +8,56 @@
 
 import UIKit
 
-class ShoppingListController : UITableViewController {
+class ShoppingListController : UIViewController, UITableViewDataSource, UITableViewDelegate  {
+    
+    @IBOutlet weak var addButton: UIBarButtonItem!
+    @IBOutlet weak var quickAddButton: UIButton!
+    @IBOutlet weak var quickAddTextField: UITextField!
+    @IBOutlet weak var shoppingListTableView: UITableView!
+    
+    var items = [Item]()
+    
+    func loadSampleData() {
+        var item1 = Item(id: 0, order: 0, name: "asdf")
+        var item2 = Item(id: 1, order: 1, name: "qwerty")
+        var item3 = Item(id: 2, order: 2, name: "qwertz")
+        
+        items += [item1, item2, item3]
+    }
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        shoppingListTableView.delegate = self
+        shoppingListTableView.dataSource = self
+        
+        loadSampleData()
+    }
     
     
+    // MARK: - Table view data source
+    
+    /*override*/ func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // Return the number of sections.
+        return 1
+    }
+    
+    /*override*/ func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // Return the number of rows in the section.
+        return items.count
+    }
+    
+    
+    /*override*/ func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        // Table view cells are reused and should be dequeued using a cell identifier.
+        let cellIdentifier = "ItemTableViewCell"
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) /*as! UITableViewCell*/ as! ItemTableViewCell
+        
+        let item = items[indexPath.row]
+        
+        cell.nameLabel.text = item.name
+        return cell
+    }
+
 }
