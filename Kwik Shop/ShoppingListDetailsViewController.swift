399937c0083d2a4a53c64d8bcee62e1fc738e6b1
Adrian Kretschmer <adrian@kremoe.de>
2015-08-08 11:12:24 +0200

    added basic version of shopping list details view

---
+++ Kwik Shop/ShoppingListDetailsViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ShoppingListDetailsViewController.swift
+//  Kwik Shop
+//
+//  Created by Adrian Kretschmer on 08.08.15.
+//  Copyright (c) 2015 FAU-Inf2. All rights reserved.
+//
+
+import UIKit
+
+class ShoppingListDetailsViewController: UIViewController {
+    
+    // MARK: Properties
+    @IBOutlet weak var cancelButton: UIBarButtonItem!
+    @IBOutlet weak var doneButton: UIBarButtonItem!
+    @IBOutlet weak var toolbar: UIToolbar!
+    @IBOutlet weak var deleteButton: UIBarButtonItem!
+    @IBOutlet weak var nameTextField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        toolbar.hidden = true
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
