3b0eaffad899c2567790d23590f22c9b86e4e01e
Adrian Kretschmer <adrian@kremoe.de>
2015-08-10 17:47:01 +0200

    split the items list in ShoppingList.swift into a bought and a not-bought list. The old item property is now a computed property which returns a concatenation of the two arrays and when set splits it into the two arrays

--- Kwik Shop/ShoppingList.swift
+++ Kwik Shop/ShoppingList.swift
@@ -12,20 +12,42 @@ class ShoppingList {
     var/*let*/ id : Int?
     var name : String
     var sortType : Int?
-    var items : [Item]
+    
+    var notBoughtItems : [Item]
+    var boughtItems : [Item]
+    var items : [Item] {
+        get {
+            let notBought = notBoughtItems ?? [Item]()
+            let bought = boughtItems ?? [Item]()
+            return notBought + bought
+        }
+        set (items) {
+            var notBought = [Item]()
+            var bought = [Item]()
+            for item in items {
+                if item.bought {
+                    bought.append(item)
+                } else {
+                    notBought.append(item)
+                }
+            }
+            self.notBoughtItems = notBought
+            self.boughtItems = bought
+        }
+    }
+    
     var lastModifiedDate : NSDate
     
-    init (id : Int, name : String, sortType : Int) {
+    init (id : Int?, name : String, sortType : Int?) {
         self.id = id;
         self.name = name;
         self.sortType = sortType
-        self.items = [Item]()
+        self.notBoughtItems = [Item]()
+        self.boughtItems = [Item]()
         self.lastModifiedDate = NSDate()
     }
     
-    init (name: String) {
-        self.name = name
-        self.items = [Item]()
-        self.lastModifiedDate = NSDate()
+    convenience init (name: String) {
+        self.init(id: nil, name: name, sortType: nil)
     }
 }
