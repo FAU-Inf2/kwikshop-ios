ca6451ab4a7aad5ff25c180e392d9fd3138b6674
Adrian Kretschmer <adrian@kremoe.de>
2015-08-10 19:47:39 +0200

    started with implementing swipe functionality

--- Kwik Shop/ShoppingListViewController.swift
+++ Kwik Shop/ShoppingListViewController.swift
@@ -136,6 +136,34 @@ class ShoppingListViewController : UIViewController, UITableViewDataSource, UITa
         return cell
     }
     
+    func tableView(tableView: UITableView, editActionsForRowAtIndexPath indexPath: NSIndexPath) -> [AnyObject]? {
+        let more = UITableViewRowAction(style: .Normal, title: "More") { action, index in
+            println("more button tapped")
+        }
+        more.backgroundColor = UIColor.lightGrayColor()
+        
+        let favorite = UITableViewRowAction(style: .Normal, title: "Favorite") { action, index in
+            println("favorite button tapped")
+        }
+        favorite.backgroundColor = UIColor.orangeColor()
+        
+        let share = UITableViewRowAction(style: .Normal, title: "Share") { action, index in
+            println("share button tapped")
+        }
+        share.backgroundColor = UIColor.blueColor()
+        
+        return [share, favorite, more]
+    }
+    
+    func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // the cells you would like the actions to appear needs to be editable
+        return true
+    }
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        // you need to implement this method too or you can't swipe to display the actions
+    }
+    
     private func getIndexForIndexPath(indexPath: NSIndexPath) -> Int {
         if indexPath.row < items.count {
             return indexPath.row
