7d77f931dc0612a54fef390362b1fee8f05cbe00
Adrian Kretschmer <adrian@kremoe.de>
2015-08-16 12:06:31 +0200

    removed id from Unit

--- Kwik Shop/Unit.swift
+++ Kwik Shop/Unit.swift
@@ -10,18 +10,17 @@ import Foundation
 
 class Unit : Equatable{
     
-    static let BAG = Unit(id: 0, name: "unit_bag")
-    static let BOTTLE = Unit(id: 1, name: "unit_bottle")
-    static let BOX = Unit(id: 2, name: "unit_box")
-    static let DOZEN = Unit(id: 3, name: "unit_dozen")
-    static let GRAM = Unit(id: 4, name: "unit_gram", shortName: "unit_gram_short")
-    static let KILOGRAM = Unit(id: 5, name: "unit_kilogram", shortName: "unit_kilogram_short")
-    static let LITRE = Unit(id: 6, name: "unit_litre", shortName: "unit_litre_short")
-    static let MILLILITRE = Unit(id: 7, name: "unit_millilitre", shortName: "unit_millilitre_short")
-    static let PACK = Unit(id: 8, name: "unit_pack")
-    static let PIECE = Unit(id: 9, name: "unit_piece", shortName: "unit_piece_short")
+    static let BAG =        Unit(name: "unit_bag")
+    static let BOTTLE =     Unit(name: "unit_bottle")
+    static let BOX =        Unit(name: "unit_box")
+    static let DOZEN =      Unit(name: "unit_dozen")
+    static let GRAM =       Unit(name: "unit_gram", shortName: "unit_gram_short")
+    static let KILOGRAM =   Unit(name: "unit_kilogram", shortName: "unit_kilogram_short")
+    static let LITRE =      Unit(name: "unit_litre", shortName: "unit_litre_short")
+    static let MILLILITRE = Unit(name: "unit_millilitre", shortName: "unit_millilitre_short")
+    static let PACK =       Unit(name: "unit_pack")
+    static let PIECE =      Unit(name: "unit_piece", shortName: "unit_piece_short")
     
-    let id : Int?
     let name : String
     private let short : String?
     
@@ -34,27 +33,23 @@ class Unit : Equatable{
         }
     }
     
-    init(id : Int?, name : String, shortName short : String?){
-        self.id = id
+    init(name : String, shortName short : String?){
         self.name = name
         self.short = short
     }
     
-    convenience init(id : Int?, name : String) {
-        self.init(id: id, name: name, shortName: nil)
+    convenience init(name : String) {
+        self.init(name: name, shortName: nil)
     }
     
     convenience init(managedUnit: ManagedUnit) {
         let name = managedUnit.valueForKey("name") as! String
         let shortName = managedUnit.valueForKey("shortName") as? String
-        self.init(id: nil, name: name, shortName: shortName)
+        self.init(name: name, shortName: shortName)
     }
 }
 
 func == (left: Unit, right: Unit) -> Bool {
-    if left.id != nil && left.id == right.id {
-        return true
-    }
     if left.name != right.name {
         return false
     }
