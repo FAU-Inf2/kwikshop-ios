a5be7676a44791a791c7e00945e98629d647b77f
Adrian Kretschmer <adrian@kremoe.de>
2015-08-16 11:17:24 +0200

    changed "amout" to amount

--- ManagedItem.swift
+++ ManagedItem.swift
@@ -2,7 +2,7 @@
 //  Item.swift
 //  Kwik Shop
 //
-//  Created by Adrian Kretschmer on 15.08.15.
+//  Created by Adrian Kretschmer on 16.08.15.
 //  Copyright (c) 2015 FAU-Inf2. All rights reserved.
 //
 
@@ -11,12 +11,12 @@ import CoreData
 
 class ManagedItem: NSManagedObject {
 
-    @NSManaged var name: String
-    @NSManaged var amout: NSNumber
-    @NSManaged var highlighted: NSNumber
+    @NSManaged var amount: NSNumber
+    @NSManaged var bought: NSNumber
     @NSManaged var brand: String
     @NSManaged var comment: String
-    @NSManaged var bought: NSNumber
+    @NSManaged var highlighted: NSNumber
+    @NSManaged var name: String
     @NSManaged var order: NSNumber
     @NSManaged var group: NSManagedObject
     @NSManaged var unit: NSManagedObject
