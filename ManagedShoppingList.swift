c204e632a8a37fdc15eff6829b7fbada3b93d5e9
Adrian Kretschmer <adrian@kremoe.de>
2015-08-16 16:21:33 +0200

    removed and re-created managed shopping list, but that didn't fix the problem

--- ManagedShoppingList.swift
+++ ManagedShoppingList.swift
@@ -1,8 +1,8 @@
 //
-//  ShoppingList.swift
+//  ManagedShoppingList.swift
 //  Kwik Shop
 //
-//  Created by Adrian Kretschmer on 15.08.15.
+//  Created by Adrian Kretschmer on 16.08.15.
 //  Copyright (c) 2015 FAU-Inf2. All rights reserved.
 //
 
@@ -11,12 +11,11 @@ import CoreData
 
 class ManagedShoppingList: NSManagedObject {
 
+    @NSManaged var lastModifiedDate: NSDate
     @NSManaged var name: String
     @NSManaged var sortType: NSNumber
-    @NSManaged var lastModifiedDate: NSDate
     @NSManaged var boughtItems: NSSet
     @NSManaged var notBoughtItems: NSSet
-    
+
     var shoppingList : ShoppingList?
-    
 }
