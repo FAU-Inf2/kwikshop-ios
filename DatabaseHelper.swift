17785dfb6fa1446e1428b54f1144fe7c8315c1e1
Adrian Kretschmer <adrian@kremoe.de>
2015-08-17 12:20:32 +0200

    made db helper a singleton instead of using static methods

--- DatabaseHelper.swift
+++ DatabaseHelper.swift
@@ -12,58 +12,30 @@ import UIKit
 
 class DatabaseHelper: NSObject {
     
-    private static var managedObjectContext : NSManagedObjectContext?
+    private let managedObjectContext : NSManagedObjectContext
     
-    /*static func getManagedObjectContext(appDelegate : AppDelegate?) -> NSManagedObjectContext?{
-        if let context = managedObjectContext {
-            return context
-        }
-        if let delegate = appDelegate {
-            managedObjectContext = delegate.managedObjectContext
-            return managedObjectContext
-        }
-        
-        if let myAppDelegate = UIApplication.sharedApplication().delegate as? AppDelegate {
-            let managedObjectContext = myAppDelegate.managedObjectContext
-            self.managedObjectContext = managedObjectContext
-        }
-        return self.managedObjectContext
-    }*/
+    static let instance = DatabaseHelper()
     
-    private static func getManagedObjectContext() -> NSManagedObjectContext?{
-        if let context = managedObjectContext {
-            return context
-        }
-        if let myAppDelegate = UIApplication.sharedApplication().delegate as? AppDelegate {
-            let managedObjectContext = myAppDelegate.managedObjectContext
-            self.managedObjectContext = managedObjectContext
-        }
-        return self.managedObjectContext
+    private override init() {
+        let myAppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        let managedObjectContext = myAppDelegate.managedObjectContext
+        self.managedObjectContext = managedObjectContext!
+        super.init()
     }
-
-    /*static func initWithAppDelegate(appDelegate : AppDelegate) {
-        if managedObjectContext == nil {
-            managedObjectContext = appDelegate.managedObjectContext
-        }
-    }*/
     
-    static func loadShoppingLists() -> [ShoppingList]? {
+    func loadShoppingLists() -> [ShoppingList]? {
         let fetchRequest = NSFetchRequest(entityName: "ShoppingList")
-        let managedObjectContext = DatabaseHelper.getManagedObjectContext()
         var result : [ShoppingList]? = nil
         
         // Execute the fetch request, and cast the results to an array of ShoppingList objects
-        if let fetchResults = managedObjectContext?.executeFetchRequest(fetchRequest, error: nil) as? [ManagedShoppingList] {
+        if let fetchResults = managedObjectContext.executeFetchRequest(fetchRequest, error: nil) as? [ManagedShoppingList] {
             let shoppingLists = ShoppingList.getShoppingListsFromManagedShoppingLists(fetchResults)
             result = shoppingLists
         }
         return result
     }
     
-    static func saveData() -> Bool {
-        if let managedObjectContext = getManagedObjectContext() {
-            return managedObjectContext.save(nil)
-        }
-        return false
+    func saveData() -> Bool {
+        return managedObjectContext.save(nil)
     }
 }
