531746f5fc8f27c66106d323f28a660f8b06b485
Adrian Kretschmer <adrian@kremoe.de>
2015-08-20 12:03:26 +0200

    Added a class and entity for autocompletion of brands

---
+++ Kwik Shop/AutoCompletionBrandData.swift
@@ -0,0 +1,16 @@
+//
+//  AutoCompletionBrandData.swift
+//  Kwik Shop
+//
+//  Created by Adrian Kretschmer on 20.08.15.
+//  Copyright (c) 2015 FAU-Inf2. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class AutoCompletionBrandData: NSManagedObject {
+
+    @NSManaged var brand: String
+
+}
