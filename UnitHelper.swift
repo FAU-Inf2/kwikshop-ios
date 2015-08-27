557fa1c9f623a1bf552d4881b49b71dc6668ee80
Adrian Kretschmer <adrian@kremoe.de>
2015-08-27 14:55:37 +0200

    Added some additional amount index types and set the types of some units to the new types if appropriate

--- UnitHelper.swift
+++ UnitHelper.swift
@@ -102,22 +102,39 @@ class UnitHelper{
         BOTTLE =     Unit(name: "unit_bottle", singularName: "unit_bottle_singular", allowedPickerIndexType: AmountIndexType.typical)
         BOX =        Unit(name: "unit_box", singularName: "unit_box_singular", allowedPickerIndexType: AmountIndexType.typical)
         DOZEN =      Unit(name: "unit_dozen", singularName: "unit_dozen_singular", allowedPickerIndexType: AmountIndexType.typical)
-        GRAM =       Unit(name: "unit_gram", singularName: "unit_gram_singular", shortName: "unit_gram_short", allowedPickerIndexType: AmountIndexType.typical)
-        KILOGRAM =   Unit(name: "unit_kilogram", singularName: "unit_kilogram_singular", shortName: "unit_kilogram_short", allowedPickerIndexType: AmountIndexType.typical)
-        LITRE =      Unit(name: "unit_litre", singularName: "unit_litre_singular", shortName: "unit_litre_short", allowedPickerIndexType: AmountIndexType.typical)
-        MILLILITRE = Unit(name: "unit_millilitre", singularName: "unit_millilitre_singular", shortName: "unit_millilitre_short", allowedPickerIndexType: AmountIndexType.typical)
+        GRAM =       Unit(name: "unit_gram", singularName: "unit_gram_singular", shortName: "unit_gram_short", allowedPickerIndexType: AmountIndexType.alsoBigValues)
+        KILOGRAM =   Unit(name: "unit_kilogram", singularName: "unit_kilogram_singular", shortName: "unit_kilogram_short", allowedPickerIndexType: AmountIndexType.onlySmallValues)
+        LITRE =      Unit(name: "unit_litre", singularName: "unit_litre_singular", shortName: "unit_litre_short", allowedPickerIndexType: AmountIndexType.onlySmallValues)
+        MILLILITRE = Unit(name: "unit_millilitre", singularName: "unit_millilitre_singular", shortName: "unit_millilitre_short", allowedPickerIndexType: AmountIndexType.alsoBigValues)
         PACK =       Unit(name: "unit_pack", singularName: "unit_pack_singular", allowedPickerIndexType: AmountIndexType.typical)
         PIECE =      Unit(name: "unit_piece", singularName: "unit_piece_singular", shortName: "unit_piece_short", allowedPickerIndexType: AmountIndexType.typical)
     }
 }
 
 enum AmountIndexType: Int {
-    case typical = 1
+    case typical = 1, onlySmallValues, alsoBigValues
+    
+    private var TYPICAL : [Int] {
+        return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 15, 20, 25, 30, 40, 50, 60, 70, 75, 80, 90, 100, 125, 150, 175, 200, 250, 300, 350, 400, 450, 500, 600, 700, 750, 800, 900, 1000]
+    }
+    
+    private var ONLY_SMALL : [Int] {
+        return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 25, 30, 35, 40, 45, 50, 75, 100]
+    }
+    
+    private var ALSO_BIG : [Int] {
+        return TYPICAL + [1250, 1500, 1750, 2000, 2250, 2500, 2750, 3000, 3500, 4000, 4500, 5000, 6000, 7000, 8000, 9000, 10000]
+    }
     
     var indices : [Int] {
         get {
             switch self {
-            case .typical: return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 15, 20, 25, 30, 40, 50, 60, 70, 75, 80, 90, 100, 125, 150, 175, 200, 250, 300, 350, 400, 450, 500, 600, 700, 750, 800, 900, 1000]
+            case .typical:
+                return TYPICAL
+            case .onlySmallValues:
+                return ONLY_SMALL
+            case .alsoBigValues:
+                return ALSO_BIG
             }
         }
     }
