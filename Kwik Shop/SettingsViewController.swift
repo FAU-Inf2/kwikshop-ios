f0c1a44f2e357eaa61341898422b96717a193a6f
Adrian Kretschmer <adrian@kremoe.de>
2015-09-02 12:16:25 +0200

    removed the old settings view + controller and renamed the new one (removed "alternative")

--- Kwik Shop/SettingsViewController.swift
+++ Kwik Shop/SettingsViewController.swift
@@ -8,60 +8,91 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+class SettingsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     // MARK: Properties
-    @IBOutlet weak var languageLabel: UILabel!
-    @IBOutlet weak var languagePicker: UIPickerView!
-    @IBOutlet weak var manageAutocomletionHistoryButton: UIButton!
-    @IBOutlet weak var manageAutocompletionBrandHistoryButton: UIButton!
+    @IBOutlet weak var settingsTableView : UITableView!
     
-    private var languageStrings : [String]!
-    private let languageAbbreviations = ["en", "de", "pt"]
-    private var selectedLanguageIndex : Int!
-    private let languageIndexKey = "languageIndex"
-
+    private let LANGUAGE_SECTION = 0
+    private let AUTOCOMPLETION_SECTION = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        settingsTableView.delegate = self
+        settingsTableView.dataSource = self
+        
         self.title = "navigation_bar_settings".localized
-        self.languageStrings = ["settings_english".localized, "settings_german".localized, "settings_portuguese".localized]
-        languageLabel.text = "settings_language".localized
-        manageAutocomletionHistoryButton.setTitle("settings_manage_autocompletion_history".localized, forState: UIControlState.Normal)
-        manageAutocompletionBrandHistoryButton.setTitle("settings_manage_brand_autocompletion_history".localized, forState: UIControlState.Normal)
         
-        languagePicker.delegate = self
-        languagePicker.dataSource = self
-        
-        let languageAbbreviation : String
-        if let storedLanguageAbbreviation = NSUserDefaults.standardUserDefaults().stringArrayForKey("AppleLanguages")?[0] as? String {
-            if let index = find(languageAbbreviations, storedLanguageAbbreviation) {
-                languageAbbreviation = storedLanguageAbbreviation
-                selectedLanguageIndex = index
-            } else {
-                // found language is not supported
-                languageAbbreviation = getPreferredLanguageOrEnglish()
-                selectedLanguageIndex = 0
-                NSUserDefaults.standardUserDefaults().setObject(["en"], forKey: "AppleLanguages")
-            }
-        } else {
-            // no language stored before
-            languageAbbreviation = getPreferredLanguageOrEnglish()
-            selectedLanguageIndex = 0
-            NSUserDefaults.standardUserDefaults().setObject(["en"], forKey: "AppleLanguages")
-        }
-        
-        languagePicker.selectRow(selectedLanguageIndex, inComponent: 0, animated: false)
-    
     }
-
-    private func getPreferredLanguageOrEnglish() -> String {
-        if let systemLanguage = NSLocale.preferredLanguages()[0] as? String {
-            if contains(self.languageAbbreviations, systemLanguage) {
-                return systemLanguage
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        if let indexPath = settingsTableView.indexPathForSelectedRow() {
+            settingsTableView.deselectRowAtIndexPath(indexPath, animated: animated)
+        }
+        settingsTableView.reloadRowsAtIndexPaths([NSIndexPath(forRow: 0, inSection: LANGUAGE_SECTION)], withRowAnimation: .None)
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // Return the number of sections.
+        return 2
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // Return the number of rows in the section.
+        if section == LANGUAGE_SECTION {
+            return 1
+        }
+        if section == AUTOCOMPLETION_SECTION {
+            return 2
+        }
+        
+        return 0
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let identifier : String
+        let row = indexPath.row
+        let section = indexPath.section
+        
+        if section == LANGUAGE_SECTION {
+            identifier = "languageSettingsCell"
+        } else /*if section == AUTOCOMPLETION_SECTION*/ {
+            if row == 0 {
+                identifier = "autocompletionSettingsCell"
+            } else /*if row == 1*/ {
+                identifier = "brandAutocompletionSettingsCell"
             }
         }
-        return "en"
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier(identifier, forIndexPath: indexPath) as! UITableViewCell
+        
+        cell.textLabel?.numberOfLines = 2
+        
+        if section == LANGUAGE_SECTION {
+            cell.textLabel?.text = "settings_language".localized
+            cell.detailTextLabel?.text = LanguageHelper.instance.selectedLanguage
+        } else {
+            if row == 0 {
+                cell.textLabel?.text = "settings_manage_autocompletion_history".localized
+            } else if row == 1 {
+                cell.textLabel?.text = "settings_manage_brand_autocompletion_history".localized
+            }
+            cell.detailTextLabel?.text = ""
+        }
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == LANGUAGE_SECTION {
+            return "settings_language".localized
+        }
+        if section == AUTOCOMPLETION_SECTION {
+            return "Autocompletion"
+        }
+        
+        return nil
     }
     
     override func didReceiveMemoryWarning() {
@@ -69,27 +100,38 @@ class SettingsViewController: UIViewController, UIPickerViewDataSource, UIPicker
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
-        if let destinationController = segue.destinationViewController as? ManageAutoCompletionHistoryViewController {
-            if sender === manageAutocomletionHistoryButton {
-                destinationController.manageItemNameCompletion = true
-            } else if sender === manageAutocompletionBrandHistoryButton {
-                destinationController.manageItemNameCompletion = false
+
+        let selectedIndexPath = settingsTableView.indexPathForSelectedRow()!
+        let row = selectedIndexPath.row
+        let section = selectedIndexPath.section
+        
+        if section == LANGUAGE_SECTION {
+            
+        } else if section == AUTOCOMPLETION_SECTION {
+            if let destinationController = segue.destinationViewController as? ManageAutoCompletionHistoryViewController {
+                if row == 0 {
+                    destinationController.manageItemNameCompletion = true
+                } else /*if row == 1*/ {
+                    destinationController.manageItemNameCompletion = false
+                }
             }
+
         }
     }
-
-
+    
     @IBAction func unwindToSettings(sender: UIStoryboardSegue) {
         // this method is called, if a user selects "delete all" in the manage autocompletion history screen
         let autoCompletionHelper = AutoCompletionHelper.instance
+        
+        //settingsTableView.deselectRowAtIndexPath(settingsTableView.indexPathForSelectedRow()!, animated: true)
         
         if let sourceViewController = sender.sourceViewController as? ManageAutoCompletionHistoryViewController {
             if sourceViewController.manageItemNameCompletion! {
@@ -100,46 +142,5 @@ class SettingsViewController: UIViewController, UIPickerViewDataSource, UIPicker
         }
         
         
-    }
-    
-    // MARK: UIPickerview Data Source and Delegate
-    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return languageStrings.count
-    }
-    
-    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
-        return 1
-    }
-    
-    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
-        return languageStrings[row]
-    }
-    
-    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        if row == selectedLanguageIndex {
-            return
-        }
-        
-        let message = "alert_box_change_language_confirmation_beginning".localized + languageStrings[row] + "alert_box_change_language_confirmation_end".localized + "?"
-        
-        let alert = UIAlertController(title: nil, message: message, preferredStyle: UIAlertControllerStyle.ActionSheet)
-        
-        let confirmationYesTitle = "alert_box_change_language_confirmation_yes_beginning".localized + languageStrings[row] + "alert_box_change_language_confirmation_yes_end".localized
-        
-        alert.addAction(UIAlertAction(title: confirmationYesTitle, style: .Default, handler: { [unowned self] (action: UIAlertAction!) in
-            self.selectedLanguageIndex = row
-            let languageAbbreviation = self.languageAbbreviations[row]
-            NSUserDefaults.standardUserDefaults().setObject(["\(languageAbbreviation)"], forKey: "AppleLanguages")
-        }))
-        
-        alert.addAction(UIAlertAction(title: "alert_box_cancel".localized, style: .Cancel, handler: { [unowned self] (action: UIAlertAction!) in
-            pickerView.selectRow(self.selectedLanguageIndex, inComponent: 0, animated: true)
-        }))
-        
-        alert.popoverPresentationController?.sourceRect = languagePicker.bounds
-        alert.popoverPresentationController?.sourceView = languagePicker
-        alert.popoverPresentationController?.permittedArrowDirections = UIPopoverArrowDirection.Left
-        
-        presentViewController(alert, animated: true, completion: nil)
     }
 }
