37c6eaed3aa985d3c9feaba393dec71816e6e87a
andreaskumlehn <andreas.kumlehn@qaware.de>
2015-10-11 20:19:06 +0200

    Updating deployment

--- fastlane/deploy.sh
+++ fastlane/deploy.sh
@@ -1,8 +1,7 @@
 if [ -f "../Kwik Shop.ipa" ]
 then
   echo "Uploading ipa"
-  scp ../Kwik\ Shop.ipa mad:/var/www/hockey/public/de.fau.cs.mad.kwikshop.ios/kwikshop.ipa
+  scp ../kwikshop.ipa mad:/var/www/hockey/public/de.fau.cs.mad.kwikshop.ios/kwikshop.ipa
 else
   echo "Ipa file not found! :-("
 fi
-
